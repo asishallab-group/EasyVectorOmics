07e785ae1b97e70233672cec7de5d1ce93e61b42
Aaron <aaron.schroeder@th-bingen.de>
2025-07-03 16:10:16 +0200

    Removed R Wrapper for now since it was outdated.

--- arrays/src/compile_files.sh
+++ arrays/src/compile_files.sh
@@ -10,6 +10,12 @@ gfortran -O3 -shared -fPIC -o char_deserialize.so char_deserialize_mod.f90
 gfortran -O3 -shared -fPIC -o array_utils.so array_utils.f90
 gfortran -O3 -shared -fPIC -o serialize.so serialize.f90
 
+gfortran -O3 -shared -fPIC -o arrays.so serialize.f90 \
+   int_deserialize_mod.f90 \
+   real_deserialize_mod.f90 \
+   char_deserialize_mod.f90 \
+   array_utils.f90
+   
 # compile the test program
 gfortran -O3 -o test_arrays test_arrays.f90 \
    int_deserialize_mod.f90 \
