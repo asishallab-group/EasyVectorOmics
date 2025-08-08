32de9217a613beb65c766a714ea821d171acf2a9
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2025-08-08 17:52:52 +0200

    implemented tests and R/C exposure

--- test/run_tests.f90
+++ test/run_tests.f90
@@ -6,7 +6,7 @@ program main
   use mod_test_calc_tiss_avg
   use mod_test_calc_fchange
   use mod_test_euclidean_distance
-
+  use mod_test_rap_tools_omics_vector_RAP_projection
 
   implicit none
 
@@ -74,6 +74,7 @@ contains
     call add_suite("calc_tiss_avg", run_all_tests_calc_tiss_avg, run_named_tests_calc_tiss_avg)
     call add_suite("calc_fchange", run_all_tests_calc_fchange, run_named_tests_calc_fchange)
     call add_suite("euclidean_distance", run_all_tests_euclidean_distance, run_named_tests_euclidean_distance)
+    call add_suite("rap_tools_omics_vector_RAP_projection", run_all_tests_rap_tools_omics_vector_RAP_projection, run_named_tests_rap_tools_omics_vector_RAP_projection)
   end subroutine initialize_suites
 
   !> Add a suite to the registry (grows automatically)
