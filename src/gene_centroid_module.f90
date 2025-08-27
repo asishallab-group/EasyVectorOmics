d83debe4a27ff2b3d1076a9679fc91f4a29c3b08
studfaensen <luka.faensen@th-bingen.de>
2025-08-27 14:52:55 +0200

    Removed C-Wrapper subroutine from module

--- src/gene_centroid_module.f90
+++ src/gene_centroid_module.f90
@@ -1,14 +1,13 @@
 ! Module for computing expression centroids of gene families.
 !
-! This module contains the core scientific kernel and the C-interface wrapper.
-! The R-interface wrapper is defined outside the module for compatibility.
+! This module contains the core scientific kernel. The C and R interface
+! wrappers are defined outside the module for compatibility.
 module gene_centroid_module
     use, intrinsic :: iso_fortran_env, only: int32, real64
-    use, intrinsic :: iso_c_binding, only: c_int, c_double
     implicit none
 
     private
-    public :: group_centroid, mean_vector, group_centroid_c
+    public :: group_centroid, mean_vector
 
 contains
 
@@ -90,51 +89,56 @@ contains
         end do
     end subroutine group_centroid
 
-    !> C interface for group_centroid.
-    pure subroutine group_centroid_c(vectors, d, n, gene_to_family_map, num_families, &
-                                centroid_matrix, use_all_mode, ortholog_set, &
-                                selected_indices, selected_indices_len) &
-                                bind(c, name='group_centroid_c')
-        implicit none
-        !| Dimensionality of the vectors.
-        integer(c_int), value, intent(in) :: d
-        !| Total number of genes.
-        integer(c_int), value, intent(in) :: n
-        !| Total number of families.
-        integer(c_int), value, intent(in) :: num_families
-        !| The allocated length of the 'selected_indices' array.
-        integer(c_int), value, intent(in) :: selected_indices_len
-        !| Input expression vectors (passed from C).
-        real(c_double), intent(in) :: vectors(d, n)
-        !| Array mapping gene index to family ID.
-        integer(c_int), intent(in) :: gene_to_family_map(n)
-        !| Integer flag from C (0=false, non-zero=true) to use all genes.
-        integer(c_int), value, intent(in) :: use_all_mode
-        !| Integer array from C indicating subset membership.
-        integer(c_int), intent(in) :: ortholog_set(n)
-        !| Output matrix for centroids.
-        real(c_double), intent(out) :: centroid_matrix(d, num_families)
-        !| Output array for selected indices.
-        integer(c_int), intent(out) :: selected_indices(selected_indices_len)
-
-        ! Local variables
-        logical :: use_all_mode_fortran
-        logical :: ortholog_set_fortran(n)
-        integer :: i
-
-        use_all_mode_fortran = (use_all_mode /= 0)
-        do i = 1, n
-            ortholog_set_fortran(i) = (ortholog_set(i) /= 0)
-        end do
-
-        call group_centroid(vectors, d, n, gene_to_family_map, num_families, &
-                            centroid_matrix, use_all_mode_fortran, ortholog_set_fortran, selected_indices)
-    end subroutine group_centroid_c
-
 end module gene_centroid_module
 
 ! =============================================================================
-! R Wrapper Subroutine (defined outside the module)
+! C Wrapper Subroutine
+! =============================================================================
+!> C interface wrapper for group_centroid.
+pure subroutine group_centroid_c(vectors, d, n, gene_to_family_map, num_families, &
+                                  centroid_matrix, use_all_mode, ortholog_set, &
+                                  selected_indices, selected_indices_len) &
+                                  bind(c, name='group_centroid_c')
+    use, intrinsic :: iso_c_binding, only: c_int, c_double
+    use gene_centroid_module, only: group_centroid
+    implicit none
+    !| Dimensionality of the vectors.
+    integer(c_int), value, intent(in) :: d
+    !| Total number of genes.
+    integer(c_int), value, intent(in) :: n
+    !| Total number of families.
+    integer(c_int), value, intent(in) :: num_families
+    !| The allocated length of the 'selected_indices' array.
+    integer(c_int), value, intent(in) :: selected_indices_len
+    !| Input expression vectors (passed from C).
+    real(c_double), intent(in) :: vectors(d, n)
+    !| Array mapping gene index to family ID.
+    integer(c_int), intent(in) :: gene_to_family_map(n)
+    !| Integer flag from C (0=false, non-zero=true) to use all genes.
+    integer(c_int), value, intent(in) :: use_all_mode
+    !| Integer array from C indicating subset membership.
+    integer(c_int), intent(in) :: ortholog_set(n)
+    !| Output matrix for centroids.
+    real(c_double), intent(out) :: centroid_matrix(d, num_families)
+    !| Output array for selected indices.
+    integer(c_int), intent(out) :: selected_indices(selected_indices_len)
+
+    ! Local variables
+    logical :: use_all_mode_fortran
+    logical :: ortholog_set_fortran(n)
+    integer :: i
+
+    use_all_mode_fortran = (use_all_mode /= 0)
+    do i = 1, n
+        ortholog_set_fortran(i) = (ortholog_set(i) /= 0)
+    end do
+
+    call group_centroid(vectors, d, n, gene_to_family_map, num_families, &
+                        centroid_matrix, use_all_mode_fortran, ortholog_set_fortran, selected_indices)
+end subroutine group_centroid_c
+
+! =============================================================================
+! R Wrapper Subroutine
 ! =============================================================================
 !> R interface wrapper for group_centroid.
 pure subroutine group_centroid_r(vectors, d, n, gene_to_family_map, num_families, &
