48cd6ddbf2c33d818f2ef0edc7c0024cd1f1049d
Aaron <aaron.schroeder@th-bingen.de>
2025-11-20 23:39:59 +0100

    Removed doc

--- doc/src/config.F90
+++
@@ -1,8 +0,0 @@
-module config
-  implicit none
-#ifdef DEFAULT_ALIGNMENT
-  integer, parameter :: alignment = DEFAULT_ALIGNMENT
-#else
-  integer, parameter :: alignment = 32  ! fallback
-#endif
-end module config
