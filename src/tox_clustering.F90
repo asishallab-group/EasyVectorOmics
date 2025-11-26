d5c47f9c4bdd55d53716084f4f895cc9bf99cc07
Jitu Daba <jitu.daba@th-bingen.de>
2025-11-26 17:03:43 +0100

    Import full tox_errors module to ensure explicit interfaces available during build

--- src/tox_clustering.F90
+++ src/tox_clustering.F90
@@ -4,7 +4,7 @@ module tox_clustering
     use safeguard
     use, intrinsic :: iso_fortran_env, only: int32, real64
     use, intrinsic :: ieee_arithmetic, only: ieee_value, ieee_positive_inf, ieee_is_nan
-    use tox_errors, only: is_err, set_err, set_ok, ERR_NAN_INF, ERR_INVALID_INPUT, validate_dimension_size, validate_distance_matrix, validate_all_in_range_real, validate_in_range_int
+    use tox_errors
     implicit none
 
     integer(int32), parameter :: METHOD_AVERAGE = 0
